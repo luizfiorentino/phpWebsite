436cde86b1e4b935a9f4a7e4f90815ef5dfaa112
luizfiorentino <lgfiorentino@gmail.com>
2024-11-27 14:53:54 +0100

    added userupdate handler to the user's form

--- index.php
+++ index.php
@@ -19,6 +19,24 @@ declare(strict_types=1);
         <button>Sign up</button>
 </form>
 
+<h3>Change account</h3>
+
+<form action="includes/userupdate.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+        <button>Update</button>
+</form>
+
+<h3>Delete account</h3>
+
+<form action="includes/userdelete.inc.php" method="post">
+        <input type="text" name="username" placeholder="Username">
+        <input type="text" name="pwd" placeholder="Password">
+        <input type="text" name="email" placeholder="E-mail">
+        <button>Delete</button>
+</form>
+
 </body>
 
 </html>
